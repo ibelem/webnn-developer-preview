a4fc70f85aa02983290be0b9da2c7875ab880893
Belem Zhang <belem.zhang@intel.com>
2025-03-10 10:35:01 +0800

    Update Transformers.js Dists

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
     {
-        ignores: ["assets/dist_transformers/", "assets/dist/", "demos/whisper-base/static/js/*"],
+        ignores: ["assets/assets/dist_transformers/", "assets/dist_transformers/", "assets/dist/", "demos/whisper-base/static/js/*"],
     },
     {
         languageOptions: {
